1d8202e5368f977527b0fdaf3e38371cb3537fc5
iscapel <isaac.garm2000@gmail.com>
2023-10-24 18:20:47 +0200

    Clases Enumerativas

---
+++ src/model/etiqueta.js
@@ -0,0 +1,9 @@
+const Etiqueta = {
+  B: "Etiqueta B",
+  C: "Etiqueta C",
+  ECO: "Etiqueta ECO",
+  CERO: "Etiqueta CERO",
+};
+
+// Exportar la eriqueta
+module.exports = Etiqueta;
---
+++ src/model/rol.js
@@ -0,0 +1,7 @@
+const Etiqueta = {
+  Empleado: "Empleado",
+  Cliente: "Cliente",
+};
+
+// Exportar el tipo enumerativo para que esté disponible en otros archivos
+module.exports = Etiqueta;
