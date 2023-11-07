f492206f6386340e0999ff04c0890d948ed7fb2a
PabloMartinez36 <Pablo.Martinez36@alu.uclm.es>
2023-11-07 17:45:22 +0100

    añadido clases a public model

---
+++ public/model/reserva.js
@@ -0,0 +1,57 @@
+
+
+class Reserva{
+
+    _id;
+    _inicio;
+    _fin;
+    _costo;
+    _numero;
+    _entrega;
+    _devolucion;
+    _fecha;
+    _clienteId;
+    _vehiculoId;
+
+    constructor(_id, inicio, fin, costo, numero, entrega, devolucion, fecha, clienteId, vehiculoId) {
+        this._id = _id;
+        this._inicio = inicio; 
+        this._fin = fin;
+        this._costo = costo;
+        this._numero = numero;
+        this._entrega = entrega;
+        this._devolucion = devolucion;
+        this._fecha = fecha;
+        this._clienteId = clienteId;
+        this._vehiculoId = vehiculoId;
+    }
+    
+    set inicio(inicio) { this._inicio = inicio; }
+    set fin(fin) { this._fin = fin; }
+    set costo(costo) { this._costo = costo; }
+    set numero(numero) { this._numero = numero; }
+    set entrega(entrega) { this._entrega = entrega; }
+    set devolucion(devolucion) { this._devolucion = devolucion; }
+    set fecha(fecha) { this._fecha = fecha; }
+    /**
+     * @param {string} clienteId
+     */
+    set clienteId(clienteId) { this._clienteId = clienteId; }
+    /**
+     * @param {any} vehiculoId
+     */
+    set vehiculoId(vehiculoId) { this._vehiculoId = vehiculoId; }
+    
+    get inicio() { return this._inicio; }
+    get fin() { return this._fin; }
+    get costo() { return this._costo; }
+    get numero() { return this._numero; }
+    get entrega() { return this._entrega; }
+    get devolucion() { return this._devolucion; }
+    get fecha() { return this._fecha; }
+    get _clienteId() { return this._clienteId; }
+    get _vehiculoId() { return this._vehiculoId; }
+
+
+}
+    module.exports = Reserva;
---
+++ public/model/vehiculo.js
@@ -0,0 +1,49 @@
+class Vehiculo{
+    _id; 
+    _matricula; 
+    _marca; 
+    _modelo; 
+    _etiqueta; 
+    _tipo; 
+    _disponible;
+    _eliminado; 
+    _costoDia; 
+    _descripcion;
+
+ 
+
+    get matricula() { return this._matricula; }
+    get marca() { return this._marca; }
+    get modelo() { return this._modelo; }
+    get etiqueta() { return this._etiqueta; }
+    get tipo() { return this._tipo; }
+    get disponible() { return this._disponible; }
+    get eliminado() { return this._eliminado; }
+    get costoDia() { return this._costoDia; }
+    get descripcion() { return this._descripcion; }
+
+    set matricula(matricula) { this._matricula = matricula; }
+    set marca(marca) { this._marca = marca; }
+    set modelo(modelo) { this._modelo = modelo; }
+    set etiqueta(etiqueta) { this._etiqueta = etiqueta; }
+    set tipo(tipo) { this._tipo = tipo; }
+    set disponible(disponible) { this._disponible = disponible; }
+    set eliminado(eliminado) { this._eliminado = eliminado; }
+    set costoDia(costoDia) { this._costoDia = costoDia; }
+    set descripcion(descripcion) { this._descripcion = descripcion; }
+
+    constructor(_id, matricula, marca, modelo, etiqueta, tipo, disponible, eliminado, costoDia, descripcion){ 
+        this._id =_id;
+        this._matricula = matricula; 
+        this._marca = marca;
+        this._modelo = modelo;
+        this._etiqueta = etiqueta;
+        this._tipo = tipo;
+        this._disponible = disponible;
+        this._eliminado = eliminado;
+        this._costoDia = costoDia;
+        this._descripcion = descripcion;
+    }
+
+} 
+module.exports = Vehiculo;
