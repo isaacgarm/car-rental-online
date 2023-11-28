d166b880727142ec82ab9df030fc2c515942ad21
PabloMartinez36 <Pablo.Martinez36@alu.uclm.es>
2023-11-28 11:40:34 +0100

    Cambios home page

--- public/components/cliente-home-page/cliente-home-page-controller.js
+++ public/components/cliente-home-page/cliente-home-page-controller.js
@@ -3,4 +3,36 @@ class ClienteHomePageController extends PageController {
     super(model);
     this.view = new ClienteHomePageController();
   }
+  get rolUsuario() {
+    return this.view.rolUsuarioSelectValue;
+  }
+
+  get usuarioDni() {
+    return this.view.usuarioDniInputValue;
+  }
+
+  get nombre() {
+    return this.view.nombreInputValue;
+  }
+
+  get apellido() {
+    return this.view.apellidoInputValue;
+  }
+
+  get email() {
+    return this.view.emailInputValue;
+  }
+
+  get telefono() {
+    return this.view.telefonoInputValue;
+  }
+
+  get direccion() {
+    return this.view.direccionInputValue;
+  }
+
+  get contrasena() {
+    return this.view.contrasenaInputValue;
+  }
+
 }
