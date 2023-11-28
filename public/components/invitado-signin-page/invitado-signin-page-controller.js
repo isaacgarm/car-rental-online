907d3fba762b9a3ada79ec3c15d00a4b4f628dbd
iscapel <isaac.garm2000@gmail.com>
2023-11-28 19:02:34 +0100

    Signin Finalizado

--- public/components/invitado-signin-page/invitado-signin-page-controller.js
+++ public/components/invitado-signin-page/invitado-signin-page-controller.js
@@ -3,22 +3,32 @@ class InvitadoSigninPageController extends PageController {
     super(model);
     this.view = new InvitadoSigninPageView();
   }
-  get usuarioRol() { return this.view.usuarioRolInputValue; }
-  get usuarioEmail() { return this.view.usuarioEmailInputValue; }
-  get usuarioPassword() { return this.view.usuarioPasswordInputValue; }
+  get usuarioRol() {
+    return this.view.usuarioRolInputValue;
+  }
+  get usuarioEmail() {
+    return this.view.usuarioEmailInputValue;
+  }
+  get usuarioPassword() {
+    return this.view.usuarioPasswordInputValue;
+  }
   async signin(event) {
     event.preventDefault();
     this.view.form.reportValidity();
     let valid = this.view.form.checkValidity();
-    if(valid){
-      let usuario = this.model.signin(this.usuarioRol,this.usuarioEmail,this.usuarioPassword);
-    }
+    if (valid) {
+      this.model.signin(
+        this.usuarioRol,
+        this.usuarioEmail,
+        this.usuarioPassword
+      );
 
-    if (usarioRol === "Cliente"){
-        event.target.href = "cliente-home-page";         
-    }else{
-        event.target.href = "empleado-home-page";
+      if (usarioRol === "Cliente") {
+        event.target.href = "/car-rental-online/cliente-home-page";
+      } else {
+        event.target.href = "/car-rental-online/empleado-home-page";
+      }
+      router.route(event);
     }
-    router.route(event);
   }
 }
