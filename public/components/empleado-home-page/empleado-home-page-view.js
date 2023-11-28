184b11b698a0a1a1c2642f6de34d32d94866df1e
iscapel <isaac.garm2000@gmail.com>
2023-11-28 12:29:35 +0100

    Arreglados HomePage

--- public/components/empleado-home-page/empleado-home-page-view.js
+++ public/components/empleado-home-page/empleado-home-page-view.js
@@ -1,6 +1,8 @@
-class EmpleadoHomePageController extends PageController {
-  constructor(model) {
-      super(model);
-      this.view = new EmpleadoHomePageView();
+class EmpleadoHomePageView extends PageView {
+  constructor() {
+    super("empleado-home-page");
+  }
+  async refresh(url) {
+    await super.refresh(url);
   }
 }
