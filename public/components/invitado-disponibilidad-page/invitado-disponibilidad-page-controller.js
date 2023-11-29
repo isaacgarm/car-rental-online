fc4eb6aa79081465f5d4129331b4d5016c2b6315
Aljndr010 <145481325+Aljndr010@users.noreply.github.com>
2023-11-30 00:01:38 +0100

    drg

--- public/components/invitado-disponibilidad-page/invitado-disponibilidad-page-controller.js
+++ public/components/invitado-disponibilidad-page/invitado-disponibilidad-page-controller.js
@@ -1,4 +1,4 @@
-class InvitadodisponibilidadPageController extends PageController {
+/*class InvitadodisponibilidadPageController extends PageController {
     constructor(model) {
       super(model);
       this.view = new InvitadoDisponibilidadPageView();
@@ -6,3 +6,40 @@ class InvitadodisponibilidadPageController extends PageController {
     getReservas() { return this.model.reservas; }
     getVehiculos() { return this.model.vehiculos; }
 }
+*/
+
+// Importa la clase Vehiculo si no lo has hecho antes
+const Vehiculo = require('./ruta-a-vehiculo-modelo');
+
+class InvitadoDisponibilidadPageController {
+  constructor(carRentalOnline) {
+    this.carRentalOnline = carRentalOnline;
+    // Utiliza el método disponibles del modelo para obtener los vehículos disponibles
+    this.vehiculosDisponibles = this.carRentalOnline.disponibles({
+      marca: 'Toyota', // Datos ficticios para probar
+      modelo: 'Corolla',
+      tipo: 'Sedán',
+      etiqueta: 'Económico',
+      costoDia: 50,
+      inicio: new Date(),
+      fin: new Date(),
+    });
+    this.render();
+  }
+
+  render() {
+    // Puedes utilizar un bucle para mostrar la información de cada vehículo en la página
+    this.vehiculosDisponibles.forEach((vehiculo) => {
+      console.log(`ID: ${vehiculo._id}, Matrícula: ${vehiculo.matricula}, Marca: ${vehiculo.marca}, Modelo: ${vehiculo.modelo}`);
+      // Aquí podrías llamar a una función que renderice la información en la vista
+    });
+  }
+
+  reservar() {
+    // Lógica para redirigir a invitado-signin-page al hacer clic en el botón Reservar
+    // Utiliza la lógica de navegación que tengas en tu aplicación
+  }
+}
+
+// Exporta la clase para ser utilizada en otras partes de tu aplicación
+module.exports = InvitadoDisponibilidadPageController;
--- public/components/invitado-disponibilidad-page/invitado-disponibilidad-page-view.js
+++ public/components/invitado-disponibilidad-page/invitado-disponibilidad-page-view.js
@@ -1,6 +1,17 @@
-class InvitadoSigninPageView extends PageView {
+/*class InvitadoSigninPageView extends PageView {
     constructor() {
         super("invitado-disponibilidad-page");
     }
 
-}
+}*/
+
+class InvitadoDisponibilidadPageView {
+    constructor(controller) {
+      this.controller = controller;
+    }
+  
+    // Puedes agregar métodos para manejar la renderización de la página si es necesario
+  }
+  
+  // Exporta la clase para ser utilizada en otras partes de tu aplicación
+  module.exports = InvitadoDisponibilidadPageView;
