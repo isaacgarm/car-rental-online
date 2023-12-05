570ce2e5c4ca02d605f7cfeb0d5b15c7c97cab5d
ALEJANDRO RAMOS MORENO <Alejandro.Ramos4@alu.uclm.es>
2023-12-05 18:16:34 +0100

    prueba arreglada(aun quedanxd)

--- public/components/empleado-agregar-vehiculo-page/empleado-agregar-vehiculo-page-view.js
+++ public/components/empleado-agregar-vehiculo-page/empleado-agregar-vehiculo-page-view.js
@@ -1,3 +1,7 @@
 class EmpleadoAgregarVehiculoPageView extends PageView{
+    constructor() { super('empleado-agregar-vehiculo-page') }
     
+    get form() { return document.getElementById('agregarFormId'); }
+    get vehiculoModeloInput() { return document.getElementById('modeloInputTextId'); }
+    get vehiculoModeloInputValue() { return this.vehiculoModeloInput.value; }
 }
