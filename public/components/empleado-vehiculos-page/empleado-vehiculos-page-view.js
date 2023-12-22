88b49d6f3fb424aa7310f3a7bded502d7b1fdb3a
ALEJANDRO RAMOS MORENO <Alejandro.Ramos4@alu.uclm.es>
2023-12-22 18:44:38 +0100

    actualizacion2

--- public/components/empleado-vehiculos-page/empleado-vehiculos-page-view.js
+++ public/components/empleado-vehiculos-page/empleado-vehiculos-page-view.js
@@ -1,3 +1,39 @@
 class EmpleadoVehiculosPageView extends PageView{
-    
+    constructor() {
+        super("empleado-vehiculos-page");
+      }
+
+      mostrarListaVehiculos(vehiculos) {
+        // Limpiar el contenedor antes de agregar los nuevos vehículos
+        this.listaVehiculosContainer.innerHTML = '';
+
+        // Iterar sobre la lista de vehículos y agregarlos al contenedor
+        vehiculos.forEach(vehiculo => {
+            const vehiculoElement = document.createElement('p');
+            vehiculoElement.textContent = `Matrícula: ${vehiculo.matricula} - Estado: ${vehiculo.estado}`;
+            
+            // Asignar color según el estado del vehículo
+            if (vehiculo.estado === 'eliminado') {
+                vehiculoElement.style.color = 'red';
+            } else if (vehiculo.estado === 'entregado') {
+                vehiculoElement.style.color = 'blue';
+            } else {
+                vehiculoElement.style.color = 'green';
+            }
+
+            // Agregar evento de clic para la navegación
+            vehiculoElement.addEventListener('click', () => {
+                this.navegarAVehiculo(vehiculo._id);
+            });
+
+            this.listaVehiculosContainer.appendChild(vehiculoElement);
+        });
+    }
+
+    // Método para navegar a la página de vehículo
+    navegarAVehiculo(vehiculoId) {
+        const url = `/empleado-vehiculo-page?vehiculoId=${vehiculoId}`;
+        window.location.href = url;
+    }
 }
+module.exports = EmpleadoVehiculosPageView;
