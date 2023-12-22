60adfb230bb430a34a46cfb3bda4e829dff4e9e9
ALEJANDRO RAMOS MORENO <Alejandro.Ramos4@alu.uclm.es>
2023-12-22 18:43:16 +0100

    acctualizacion1

--- public/components/empleado-vehiculos-page/empleado-vehiculos-page-controller.js
+++ public/components/empleado-vehiculos-page/empleado-vehiculos-page-controller.js
@@ -1,3 +1,26 @@
 class EmpleadoVehiculosPageController extends PageController{
-    
+    constructor(model) {
+        super(model);
+        this.view = new EmpleadoVehiculosPageView();
+    }
+
+    get vehiculos() {
+        return this.view.vehiculos;
+      }
+
+    mostrarTodosLosVehiculos() {
+        // Obtener todos los vehículos desde el modelo (puedes introducir datos ficticios para probar)
+        const todosLosVehiculos = this.model.getVehiculos();
+
+        // Lógica para mostrar los vehículos en la interfaz de usuario
+        this.view.mostrarListaVehiculos(todosLosVehiculos);
+    }
+
+    // Lógica para la navegación al hacer clic en la matrícula
+    navegarAVehiculo(vehiculoId) {
+        const url = `/empleado-vehiculo-page?vehiculoId=${vehiculoId}`;
+        window.location.href = url;
+    }
+
 }
+module.exports = EmpleadoVehiculosPageController;
